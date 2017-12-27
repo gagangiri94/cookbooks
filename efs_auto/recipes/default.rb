f1a1723e03a7fe6b1c68394e300ca39468f9fa56
gagangiri94 <gagan@nclouds.com>
2017-12-27 22:01:41 +0530

    test

--- efs_auto/recipes/default.rb
+++ efs_auto/recipes/default.rb
@@ -8,7 +8,7 @@ directory '/efs' do
 end
 
 mount '/efs'  do
-  fsid 'fs-938b8fda'
+  device 'fs-938b8fda'
   options ['rsize=1048576', 'wsize=1048576', 'hard', 'timeo=600', 'retrans=2']
   action :mount
 end
