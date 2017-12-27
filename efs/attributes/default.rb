dc4624598894a709d5760c4acb18d5263e1f0ba3
gagangiri94 <gagan@nclouds.com>
2017-12-28 03:04:22 +0530

    new cookbook b

--- efs/attributes/default.rb
+++ efs/attributes/default.rb
@@ -3,7 +3,11 @@ default['efs']['wsize'] = 1_048_576
 default['efs']['behavior'] = 'hard'
 default['efs']['timeout'] = 600
 default['efs']['retrans'] = 2
-default['efs']['mounts'] = {}
+default['efs']['mounts'] = {
+    "/efs" => {
+        "fsid" => "fs-938b8fda"
+    }
+}
 default['efs']['remove_unspecified_mounts'] = false
 
 default['efs']['nfs-package'] = case node['platform']
--- efs/recipes/default.rb
+++ efs/recipes/default.rb
@@ -1,7 +1,11 @@
+#
+# Cookbook Name:: efs
+# Recipe:: default
+#
+# Copyright 2017, Matt Kulka
+#
 
 package node['efs']['nfs-package']
-
-package 'nfs-utils'
 
 directory '/efs' do
   owner 'root'
@@ -9,23 +13,18 @@ directory '/efs' do
   mode '0755'
   action :create
 end
-
-mount_efs '/efs'  do
-  fsid 'fs-938b8fda'
-  options ['rsize=1048576', 'wsize=1048576', 'hard', 'timeo=600', 'retrans=2']
+ 
+node['efs']['mounts'].each do |mount_point, attribs|
+  mount_efs mount_point do
+    fsid attribs['fsid']
+    options attribs['options']
+    action :mount
+  end
 end
 
-#node['efs']['mounts'].each do |mount_point, attribs|
-#  mount_efs mount_point do
-#    fsid attribs['fsid']
-#    options attribs['options']
-#    action :mount
-#  end
-#end
-
-#ruby_block 'remove unspecified efs mounts' do
-#  only_if { node['efs']['remove_unspecified_mounts'] }
-#  block do
-#    EFS::Mount.remove_unspecified_mounts(node['efs']['mounts'], run_context)
-#  end
-#end
+ruby_block 'remove unspecified efs mounts' do
+  only_if { node['efs']['remove_unspecified_mounts'] }
+  block do
+    EFS::Mount.remove_unspecified_mounts(node['efs']['mounts'], run_context)
+  end
+end
