f2a883df680cc137f6174e17c7a0f1b7213a7e9e
gagangiri94 <gagan@nclouds.com>
2017-12-30 00:20:28 +0530

    add nclouds fsid

---
+++ efs_nclouds/recipes/default.rb
@@ -0,0 +1,29 @@
+#package 'Install NFS' do
+#  case node[:platform]
+#  when 'redhat', 'centos'
+#    package_name 'nfs-utils'
+#  when 'ubuntu', 'debian'
+#    package_name 'nfs-common'
+#  end
+#end
+package 'nfs-utils'
+
+az = node['ec2']['placement_availability_zone']
+region = az[0..-2] # trim last char
+file_system_id = 'fs-938b8fda'
+efs_uri = "#{file_system_id}.efs.#{region}.amazonaws.com:/" 
+
+directory '/efs' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
+  action :create
+end
+
+mount '/efs' do
+	device efs_uri
+    fstype 'nfs4'
+	options 'nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2'
+	action [:mount, :enable]
+end
