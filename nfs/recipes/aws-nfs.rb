c0a2b82ad2d658ce3f82169055dfbf0812f72e3c
gagangiri94 <gagan@nclouds.com>
2018-01-11 13:15:19 +0530

    new nfs

---
+++ nfs/recipes/aws-nfs.rb
@@ -0,0 +1,32 @@
+execute 'apt update' do
+  command 'apt-get update'
+  action :run
+end
+
+package 'nfs-kernel-server' do
+	action :install
+end	
+
+directory '/nfs/data/mps' do
+  owner 'nobody'
+  group 'nogroup'
+  action :create
+end 
+
+cookbook_file '/tmp/exports' do
+  source 'exports'
+end
+
+bash 'append_to_exportfile' do
+ user 'root'
+ code <<-EOH
+ cat /tmp/exports >> /etc/exports
+ rm /tmp/exports
+ EOH
+end
+
+
+execute 'restart nfs' do
+  command 'systemctl restart nfs-kernel-server'
+  action :run
+end
