fa8663c044d2481dc2720410342d2bdbf0cdbf5c
Gagan Goswami <gagan@Gagans-MacBook-Pro-2.local>
2019-09-24 21:31:01 +0530

    clean up

--- manual/recipes/nd-restart.rb
+++ manual/recipes/nd-restart.rb
@@ -24,19 +24,6 @@ execute 'start message' do
   action :run
 end
 
-# service 'httpd stop' do
-#   service_name 'apache2'
-#   supports :status => true
-#   stop_command 'service apache2 stop && sleep 50'
-#   action :stop
-# end
-
-#
-# service 'apache start' do
-#   service_name 'apache2'
-#   supports :status => true
-#   action :start
-# end
 
 service 'apache start' do
   case node['platform']
@@ -46,6 +33,6 @@ service 'apache start' do
     service_name 'apache2'
   end
   supports :status => true
-  #start_command 'service apache2 stop && sleep 10'
+
   action :start
 end
